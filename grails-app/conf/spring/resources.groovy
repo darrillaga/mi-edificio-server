f3f44b4b016d4f68b934ab5b7e78a629dcc1756d
Damián Arrillaga <damian.arrillaga@gmail.com>
2016-05-11 03:21:20 -0300

    Had to fix locale, out of memory occurring whit en-US locale

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,6 @@
+import org.springframework.web.servlet.i18n.FixedLocaleResolver
+
 // Place your Spring DSL code here
 beans = {
+    localeResolver(FixedLocaleResolver, Locale.createConstant("es", "ES"))
 }
